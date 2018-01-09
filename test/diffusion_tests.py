cbba6ba9b408b19581c7d2810923668323969c6d
Marissa Ramirez Zweiger <mzweig@berkeley.edu>
2018-01-08 19:14:53 -0800

    Diffusion testing.

---
+++ test/diffusion_tests.py
@@ -0,0 +1,26 @@
+from nose.tools import *
+import numpy as np
+import scipy.sparse as sps
+import scipy.sparse.linalg as linalg
+import sys
+sys.path.append('../src')
+
+from diffusion import Diffusion
+from fe import *
+from materials import Materials
+from plot import plot
+
+class TestDiffusion():
+    @classmethod
+    def setup_class(cls):
+        cls.nodefile = "test/test_inputs/box.node"
+        cls.elefile = "test/test_inputs/box.ele"
+        cls.matfile = "test/test_inputs/box.mat"
+        cls.fegrid = FEGrid(cls.nodefile, cls.elefile)
+        cls.materials = Materials(cls.matfile)
+        cls.operator = Diffusion(cls.fegrid, cls.materials)
+
+    def test_matrix(self):
+        A = self.operator.get_matrix()
+        assert (A!=A.transpose()).nnz==0
+        assert (A.diagonal() >= 0).all()
