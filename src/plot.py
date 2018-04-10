1cd46a0286dba0b2cb3403f8a2ac90f501b0710b
Marissa Ramirez Zweiger <mzweig@berkeley.edu>
2018-04-10 15:10:39 -0700

    Added cubic interpolation into plot.

--- src/plot.py
+++ src/plot.py
@@ -15,71 +15,20 @@ def plot(grid, solution, filename, mesh_plot=False):
     # Setup triangles
     elts = grid.get_num_elts()
     triangles = np.array([grid.element(i).get_vertices() for i in range(elts)])
+    triang = tri.Triangulation(x, y, triangles=triangles)
+    # Interpolate to Refined Triangular Grid
+    interp_lin = tri.CubicTriInterpolator(triang, solution)
+    refiner = tri.UniformTriRefiner(triang)
+    tri_refi, sol_refi = refiner.refine_field(solution, triinterpolator=interp_lin, subdiv=3)
     # Setup colorbar
-    inf = np.min(solution)
-    sup = np.max(solution)
+    inf = np.min(sol_refi)
+    sup = np.max(sol_refi)
     # Plot and save to file
     plt.figure()
-    plt.tricontourf(x, y, triangles, solution, levels=np.linspace(inf, sup, 11))
-    if mesh_plot==True:
-        plt.triplot(x, y, triangles, 'go-', lw=1.0)
+    plt.triplot(triang)
+    plt.tricontourf(tri_refi, sol_refi, levels=np.linspace(inf, sup, 11))
     plt.colorbar()
     plt.savefig(filename)
     plt.clf()
     plt.close()
 
-# def mesh_plot(grid, filename):
-#     # Get number of nodes
-#     nodes = grid.get_num_nodes()
-#     # Setup xy
-#     x = np.zeros(nodes)
-#     y = np.zeros(nodes)
-#     positions = (grid.node(i).get_position() for i in range(nodes))
-#     for i, pos in enumerate(positions):
-#         x[i], y[i] = pos
-#     # Setup triangles
-#     elts = grid.get_num_elts()
-#     triangles = np.array([grid.element(i).get_vertices() for i in range(elts)])
-#     # Plot mesh
-#     plt.figure()
-#     plt.gca().set_aspect('equal')
-#     plt.triplot(x, y, triangles, 'go-', lw=1.0)
-#     plt.savefig(filename + "_mesh")
-    
-# def plot_interior(grid, solution, filename, mesh_plot=False):
-#     # Get number of interior nodes
-#     nodes = grid.get_num_interior_nodes()
-#     # Setup xy
-#     x = np.zeros(nodes)
-#     y = np.zeros(nodes)
-#     positions = (grid.interior_node(i).get_position() for i in range(nodes))
-#     for i, pos in enumerate(positions):
-#         x[i], y[i] = pos
-#     # Setup triangles
-#     elts = grid.get_num_elts()
-#     triangles = []
-#     for i in range(elts):
-#         vertices = grid.element(i).get_vertices()
-#         is_interior = np.zeros(3)
-#         vertices_interior = np.zeros(3)
-#         for i, v in enumerate(vertices):
-#             is_interior[i] = grid.node(v).is_interior()
-#             vertices_interior[i] = grid.node(v).get_interior_node_id()
-#         if is_interior.all():
-#             triangles.append(vertices_interior)
-#     triangles = np.array(triangles)
-#     if mesh_plot:
-#         # Plot mesh
-#         plt.figure()
-#         plt.gca().set_aspect('equal')
-#         plt.triplot(x, y, triangles, 'go-', lw=1.0)
-#         plt.savefig(filename + "_mesh")
-#     # Setup colorbar
-#     inf = np.min(solution)
-#     sup = np.max(solution)
-#     # Plot and save to file
-#     plt.figure()
-#     plt.tricontourf(x, y, triangles, solution, levels=np.linspace(inf, sup, 11))
-#     plt.colorbar()
-#     plt.savefig(filename)
-#     plt.clf()
