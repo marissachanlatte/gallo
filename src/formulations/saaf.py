Fixed bug in directional derivative.

src/formulations/saaf.py
@@ -118,6 +118,12 @@ class SAAF():
             # Determine Gauss Nodes for element
             g_nodes = self.fegrid.gauss_nodes(e)
             grad = np.array([self.fegrid.gradient(e, i) for i in range(3)])
+            # Compute Phi at centroid
+            centroid = self.fegrid.centroid(e)
+            phi_centroid = np.zeros(self.num_groups)
+            for g in range(self.num_groups):
+                interp = tri.LinearTriInterpolator(triang, phi_prev[g])
+                phi_centroid[g] = interp(centroid[0], centroid[1])
             for n in range(3):
                 n_global = self.fegrid.get_node(e, n)
                 # Coefficients of basis functions b[0] + b[1]x + b[2]y
@@ -138,15 +144,19 @@ class SAAF():
                 product = fn_vals * ssource
                 ssource_integrated = self.fegrid.gauss_quad(e, product)
                 rhs_at_node[nid] += ssource_integrated / (4 * np.pi)
-                # Second Scattering Term
-                product = ssource*(angles@grad[n])
-                ssource_integrated = self.fegrid.gauss_quad(e, product)
-                rhs_at_node[nid] += inv_sigt*ssource_integrated/(4*np.pi)
                 # First Fixed Source Term
                 q_fixed = source[group_id, e] / (4 * np.pi)
                 rhs_at_node[nid] += q_fixed * (area / 3)
-                # Second Fixed Source Term
-                rhs_at_node[nid] += inv_sigt*q_fixed*(angles@grad[n])*area
+                # Directional Derivative
+                ssource = self.compute_scattering_source(midx, phi_centroid, group_id)
+                Q = (ssource + source[group_id, e])/(4*np.pi)
+                rhs_at_node[nid] += inv_sigt*Q*(angles@grad[n])*area
+                # Second Scattering Term
+                # product = ssource*(angles@grad[n])
+                # ssource_integrated = self.fegrid.gauss_quad(e, product)
+                # rhs_at_node[nid] += inv_sigt*ssource_integrated/(4*np.pi)
+                # # Second Fixed Source Term
+                # rhs_at_node[nid] += inv_sigt*q_fixed*(angles@grad[n])*area
         return rhs_at_node
 
     def compute_scattering_source(self, midx, phi, group_id):