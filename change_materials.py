c91d805787b39e3e731d0f6ecefe233f9585d801
Marissa Ramirez Zweiger <mzweig@berkeley.edu>
2018-12-01 15:18:49 -0800

    Updated to work with attrs.

--- change_materials.py
+++ change_materials.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python
 
+import attr
 from gallo import write, fe
 
 def change_material(grid, elt):
     centroid = grid.centroid(elt.el_id)
-    if all(-10/4 < p < 10/4 for p in centroid):
-        return elt
-    elif all(-5 < p < 5 for p in centroid):
-        return elt._replace(mat_id=1)
+    if all(-4 < p < 4 for p in centroid):
+        return attr.evolve(elt,mat_id=1)
     else:
         return elt
 
@@ -18,7 +17,7 @@ def main():
         ele_file="test/test_inputs/origin_centered10.ele"
     )
     elts = [change_material(grid, elt) for elt in grid.elts_list]
-    write.write_ele("iron-water10.ele", elts)
+    write.write_ele("mod-uo2.ele", elts)
 
 
 if __name__ == '__main__':
